232604f89a6785ae19b13aea876bd31192efe9f6
Matheus Telles <business.mwtelles@gmail.com>
2025-08-10 15:01:31 -0300

    updating dockerfile to run migrations on prod

--- apps/back/drizzle.config.ts
+++ apps/back/drizzle.config.ts
@@ -1,13 +1,13 @@
 import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
+const isProd = process.env.NODE_ENV === 'production';
+const schemaPath = isProd ? './dist/db/schema.js' : './src/db/schema.ts';
 
 export default {
-    schema: './src/db/schema.ts',
+    schema: schemaPath,
     out: './drizzle',
     dialect: 'postgresql',
-    dbCredentials: {
-        url: process.env.DATABASE_URL!
-    },
+    dbCredentials: { url: process.env.DATABASE_URL! },
     strict: true,
-    verbose: true
+    verbose: true,
 } satisfies Config;
