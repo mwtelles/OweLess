195ab0bcc74ee65ee85fde2bd0a55765c03f5629
Matheus Telles <business.mwtelles@gmail.com>
2025-08-10 15:16:00 -0300

    ....

--- apps/back/drizzle.config.ts
+++ apps/back/drizzle.config.ts
@@ -1,13 +1,13 @@
 import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
-const isProd = process.env.NODE_ENV === 'production';
-const schemaPath = isProd ? './dist/db/schema.js' : './src/db/schema.ts';
 
 export default {
-    schema: schemaPath,
+    schema: './src/db/schema.ts',
     out: './drizzle',
     dialect: 'postgresql',
-    dbCredentials: { url: process.env.DATABASE_URL! },
+    dbCredentials: {
+        url: process.env.DATABASE_URL!
+    },
     strict: true,
-    verbose: true,
+    verbose: true
 } satisfies Config;
